c69a138422b43fb853f1939795ccfa2cd83db1d4
Rafael Sanchez <rsanche4@stevens.edu>
2022-01-25 23:10:55 -0500

    finalized phase 1 of Zoey

--- Zoey_ChatbotApplicationSrc/README.txt
+++ Zoey_ChatbotApplicationSrc/README.txt
@@ -13,54 +13,46 @@ personality.
 
 Currently in development.
 
-Things yet to implement:
+******************************************************
+STUFF TO IMPLEMENT
 -Notes for fixer.py:
-   +The professor aiml files have some errors when being
+   + The professor aiml files have some errors when being
    parsed by the python interpreter. Change the files
    to be 1.0 compliant by writing a 'fixer.py' that
    checks for it all and changes to the correct
    version by taking out 'learn' tags and unsopported
    wildcards (^ #). 
-   +Also if finds misplaced tags delete
+   + Also if finds misplaced tags delete
    the whole category, also if it finds text inside
    tags that shouldnt be there, delete that text
- 
--If we find the <bot name=""/> tag, input the correct
- phrase. For example, name="name" should be zoey.
+   + If we find the <bot name=""/> tag, input the correct
+   phrase. For example, name="name" should be zoey.
+   + If we find the <br> tag just remove it. and Put a
+   space instead.
+   + Also, fixer should remind me of all the patterns that
+   are the same (if any two patterns are matched, please
+   let us know)
 
 -Finish editting AIML files to fit her personality
-
--Games to play with her: game of pig, blackjack, 
- rock paper scissors, war(card game), Date Me Game 
- where you go on a virtual date with her ;)
+ cool_quotes.aiml add to her
 
 -Keep a memory system where she knows about us 
- and what we like (in python). This is good because
- when we say 'bored' or something like that or she
- sends a 'im confused reply' we can tell the user
- i hread you like music. fun fact on  music: blah blah
+ and what we like (in python). If user ever says
+ what do you know about me? She will show us. How
+ will this information be used? When the bored
+ command in issued, she might say something that
+ she knows we like, ask us about that (and hopefully
+ this should steer the conversation back to her AIML
+ where she can respond based on that)
 
--Cool quotes from different media about random things
- in life. (this will be her posts in twitter as well)
+-Games to play with her: Date Me Game 
+ where you go on a virtual date with her ;)
 
--Thoughts on different anime series she is watched (Top classic series)
-
--Song of This was a triumph im making a note
- whenever a user inputs a part of the lyric
- she sings the rest
- 
--Translate to any language command
- 
--Tell me a story functionality
-
--Calendar and date functionality
-
--Make it into an exe file include a How to Use guide and
+******************************************************
+DEPLOYMENT:
+-Make it into an exe file 
+-include a How to Use guide and
  what she is capable of doing, recommandations on how
- she works best, and upload it to itch.io,
- indiedb, and other places, and perhaps STEAM but 
- under the Rafael Sanchez account. (create one)
- + On the github, create 2 folders, 1 where we have
- the src, and one with the finizaled version exe
- + Note that the final version doesnt have aiml files,
- it only has a .brn file because it's faster to load
+ she works best
+-upload it to itch.io, STEAM but 
+ under the Rafael Sanchez account.
--- Zoey_ChatbotApplicationSrc/zoey_chatbot.py
+++ Zoey_ChatbotApplicationSrc/zoey_chatbot.py
@@ -7,23 +7,20 @@ import pygame
 import random
 import getpass
 from datetime import datetime
+import calculator
+from translate import Translator
 
-# Create the kernel and learn AIML files
 kernel = aiml.Kernel()
 
-# if os.path.isfile("bot_brain.brn"):
-#     kernel.bootstrap(brainFile = "bot_brain.brn")
-# else:
-#     kernel.bootstrap(learnFiles = "brain"+os.sep+"alice"+os.sep+"*.aiml")
-#     kernel.bootstrap(learnFiles = "brain"+os.sep+"standard"+os.sep+"*.aiml")
-#     kernel.saveBrain("bot_brain.brn")
-
-
-kernel.learn("brain"+os.sep+"alicefl"+os.sep+"*.aiml")
-kernel.learn("brain"+os.sep+"standardfl"+os.sep+"*.aiml")
-kernel.learn("brain"+os.sep+"mitsukufl"+os.sep+"*.aiml")
-kernel.learn("brain"+os.sep+"zoeyfl"+os.sep+"*.aiml")
-kernel.learn("brain"+os.sep+"professorfl"+os.sep+"*.aiml")
+if os.path.isfile("brain"+os.sep+"zbrain.brn"):
+    kernel.bootstrap(brainFile = "brain"+os.sep+"zbrain.brn")
+else:
+    kernel.bootstrap(learnFiles = "brain"+os.sep+"aiml"+os.sep+"standardfl"+os.sep+"*.aiml")
+    kernel.bootstrap(learnFiles = "brain"+os.sep+"aiml"+os.sep+"alicefl"+os.sep+"*.aiml")
+    kernel.bootstrap(learnFiles = "brain"+os.sep+"aiml"+os.sep+"professorfl"+os.sep+"*.aiml")
+    kernel.bootstrap(learnFiles = "brain"+os.sep+"aiml"+os.sep+"mitsukufl"+os.sep+"*.aiml")
+    kernel.bootstrap(learnFiles = "brain"+os.sep+"aiml"+os.sep+"zoeyfl"+os.sep+"*.aiml")
+    kernel.saveBrain("brain"+os.sep+"zbrain.brn")
 
 
 x="************************************************************************"
@@ -50,14 +47,19 @@ for i in range(1, 5):
     
 
 pygame.mixer.init()
-number_of_tracks = 8
-rand = random.randint(1, number_of_tracks)
+number_of_tracks = 9
+rand = random.randint(0, number_of_tracks-1)
 pygame.mixer.music.load("gui"+os.sep+"track"+str(rand)+".mp3")
 pygame.mixer.music.play(-1)
 
 def zoey_says(m, kernel):
+    global number_of_tracks
+    global rand
     dictionary=PyDictionary()
-    if ("what is" in m.lower() or "define" in m.lower() or "definition of" in m.lower() or "meaning of" in m.lower()) and ('rafael sanchez' not in m.lower() and 'what is your' not in m.lower()):
+    if 'can you tell the time' in m.lower() or 'tell me the time' in m.lower() or 'what time is it' in m.lower() or 'what is the time' in m.lower() or 'tell me the date' in m.lower() or 'what date is today' in m.lower() or "what's the date" in m.lower() or "what day is today" in m.lower()or "what is today's date" in m.lower() or "what's today's date" in m.lower():
+        da = datetime.now().strftime("Today is: %d/%m/%Y. The time is %H:%M:%S.")
+        return da
+    elif ("what is" in m.lower() or "define" in m.lower() or "definition of" in m.lower() or "meaning of" in m.lower()) and ('rafael sanchez' not in m.lower() and 'what is your' not in m.lower()):
         r=dictionary.meaning(m.split()[-1])
         if dictionary.meaning(m.split()[-1],True) is None:
             return "I don't know... sorry."
@@ -66,62 +68,34 @@ def zoey_says(m, kernel):
         return g[0].upper()+g[1:]+"."
     elif 'shell command' in m.lower():
         os.system(m[14:])
-        return "Command executed!" 
-    elif "+" in m:
-        l=m.split('+')
-        s1=l[0].strip()
-        if s1.isnumeric():
-            n1=float(s1)
-            s2=l[1].strip()
-            if s2.isnumeric():
-                n2=float(s2)
-                return str(n1+n2)
-            else:
-                return "If you are trying to calculate something, just type in this format: number operator number (Ex: 1 + 3)"
-        else:
-            return "If you are trying to calculate something, just type in this format: number operator number (Ex: 1 + 3)"
-    elif "*" in m:
-        l=m.split('*')
-        s1=l[0].strip()
-        if s1.isnumeric():
-            n1=float(s1)
-            s2=l[1].strip()
-            if s2.isnumeric():
-                n2=float(s2)
-                return str(n1*n2)
-            else:
-                return "If you are trying to calculate something, just type in this format: number operator number (Ex: 1 + 3)"
-        else:
-            return "If you are trying to calculate something, just type in this format: number operator number (Ex: 1 + 3)"
-    elif "-" in m:
-        l=m.split('-')
-        s1=l[0].strip()
-        if s1.isnumeric():
-            n1=float(s1)
-            s2=l[1].strip()
-            if s2.isnumeric():
-                n2=float(s2)
-                return str(n1-n2)
-            else:
-                return "If you are trying to calculate something, just type in this format: number operator number (Ex: 1 + 3)"
-        else:
-            return "If you are trying to calculate something, just type in this format: number operator number (Ex: 1 + 3)"
-    elif "/" in m:
-        l=m.split('/')
-        s1=l[0].strip()
-        if s1.isnumeric():
-            n1=float(s1)
-            s2=l[1].strip()
-            if s2.isnumeric():
-                n2=float(s2)
-                if n2==0.0:
-                    return "Can't divide by zero... I think."  
-                else:
-                    return str(n1/n2)
-            else:
-                return "If you are trying to calculate something, just type in this format: number operator number (Ex: 1 + 3)"
-        else:
-            return "If you are trying to calculate something, just type in this format: number operator number (Ex: 1 + 3)"
+        return "Command executed." 
+    elif '+' in m or "-" in m or "/" in m or "*" in m:
+        return calculator.calcut(m)
+    elif ':)'== m:
+        return ';)'
+    elif ';)'== m:
+        return ':)'
+    elif 'switch music' in m.lower() or 'switch song' in m.lower() or 'change song' in m.lower() or 'change music' in m.lower() or 'switch the music' in m.lower() or 'switch the song' in m.lower() or 'change the song' in m.lower() or 'change the music' in m.lower():
+        pygame.mixer.stop()
+        rand = (rand+1) % number_of_tracks
+        pygame.mixer.music.load("gui"+os.sep+"track"+str(rand)+".mp3")
+        pygame.mixer.music.play(-1)
+        messages_song=["Changing music... there you go.", "Music changed as requested.", "Yeah... that one was getting old.", "You know, I hear these tracks in my universe everyday. Sometimes, I feel melancholic.", "Song changed!", "Digital Universe, may you please change the song? Thank you."]
+        return random.choice(messages_song)
+    elif 'translate' in m.lower():
+        if m[-1]=='.':
+            m=m.replace(".","")
+        mes_l = m.split()
+        if len(mes_l)!=4 or mes_l[0].lower()!='translate' or mes_l[2].lower()!='to':
+            return "Are you trying to translate something? To translate you should use this format: Translate <english word> to <language> (Ex: Translate hello to spanish)"
+        langl=['spanish','german','italian','french','portuguese']
+        if mes_l[3] not in langl:
+            return "I can't translate that. I only know Spanish, German, Italian, French, and Portuguese. Though I am not too comfortable speaking it."
+        translator= Translator(from_lang="english",to_lang=mes_l[3])
+        translation = translator.translate(mes_l[1])
+        finish_remark=["I could be wrong though.", "I'd check that translation with a more reliable translator.","I'm not too good at translations.", "Honestly, I don't know if I'm right."]
+        start_remark=["Perhaps: ", "Something like this: ", "Problably this: ", "I think you say it like this: ", "I'm sure is this: ", "Maybe... this: "]
+        return random.choice(start_remark)+translation+". "+random.choice(finish_remark)
     else:
         r=kernel.respond(m)
         return r
@@ -142,7 +116,7 @@ e = Entry(root, width=61, borderwidth=8, font=('Courier', 18, 'bold'), bg="black
 e.place(x=12, y=540)
 
 d3 = datetime.now().strftime("%d%m%Y%H%M%S")
-file = open("brain"+os.sep+"memory"+os.sep+"chatlog_"+d3+".txt", "a") 
+file = open("brain"+os.sep+"chatlogs"+os.sep+"conversation"+d3+".txt", "a") 
 file.write("-----------| CHAT "+d3+" STARTED |----------------------------------------------\n\n")
 
 def myClick():
@@ -154,23 +128,35 @@ def myClick():
     abel.configure(text=z_mes)
     e.delete(0, END)
 
-
-
-
 def func(event):
     myClick()
 root.bind('<Return>', func)
 
-greetings=["Hey there..."]
+greetings=["Hey there...", "How do you do?", "How have you been?", "Hello there.", "Hi!", "How are you doing?", "How's it going?", "It's great to see you.", "What's up?", "Yo!", "Lovely to see you.", "Ahoy!", "Heyo.", "HOLA."]
 displayed_message=random.choice(greetings)
 abel = Label(root,width=24,height=16, text=displayed_message,justify=LEFT,anchor=NW, wraplength=340, font=('Courier', 18, 'bold'), bg="black",fg="red", borderwidth=10, relief="ridge")
 abel.place(x=530, y=80)
+file.write("Zoey: "+displayed_message+"\n")
 
 zo = Label(root,text="ZOEY CHATBOT", width=17,font=('Courier', 23, 'bold'),bg='black',fg="violet",borderwidth=10,relief="ridge")
 zo.place(x=534, y=20)
 
 typehere = Label(root,text="Type your message here:",font=('Courier', 13, 'bold'),bg='black',fg="violet",borderwidth=3,relief="ridge")
 typehere.place(x=13, y=510)
+
+def turnoff():
+    pygame.mixer.music.stop()
+
+def turnon():
+    global number_of_tracks
+    global rand
+    pygame.mixer.music.load("gui"+os.sep+"track"+str(rand)+".mp3")
+    pygame.mixer.music.play(-1)
+
+musicTurnOff = Button(root, text="TURN OFF", command=turnoff, relief='raised',bg='black', fg="violet", borderwidth=6, font="Courier 12")
+musicTurnOff.place(x=12, y=15)
+musicTurnOn = Button(root, text="TURN  ON", command=turnon, relief='raised',bg='black', fg="violet",borderwidth=6, font="Courier 12")
+musicTurnOn.place(x=12, y=55)
 
 def on_closing():
     file.write("\n-----------| CHAT "+d3+" ENDED | CONVERSATION ARCHIVED SUCCESSFULLY |-----------\n")

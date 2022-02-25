778f4a27b7819d7b8fa86d92091d998d9a9b0156
Rafael Sanchez <rsanche4@stevens.edu>
2022-02-24 21:07:24 -0500

    created clevergirl

---
+++ Clevergirl/clevergirl.py
@@ -0,0 +1,46 @@
+# Author: Rafael Sanchez
+# Desc: Part of Clevergirl Brain in python. Works together with brain.json.
+import json
+
+def girl_says(m):
+    m=m.replace('?', '')
+    m=m.replace('!', '')
+    m=m.replace(',', '')
+    m=m.replace('.', '')
+    m=m.replace("'", '')
+    m=m.strip() 
+    m=m.upper()
+    with open('brain.json', 'r+') as f:
+        data = json.load(f)
+        for pattern, reply in data.items():
+            if m==pattern:
+                return reply
+        learn_str = input("TYPE WHAT I SHOULD SAY FOR THAT-> ")
+        learn_str=learn_str.replace('?', '')
+        learn_str=learn_str.replace('!', '')
+        learn_str=learn_str.replace(',', '')
+        learn_str=learn_str.replace('.', '')
+        learn_str=learn_str.replace("'", '')
+        learn_str=learn_str.strip() 
+        learn_str=learn_str.upper()
+        string = '{"'+m+'": "'+learn_str+'"}'
+        new_stuff=json.loads(string)
+        data.update(new_stuff)
+        f.seek(0)
+        json.dump(data, f, indent = 1)
+        return learn_str
+
+print("")
+print("")
+print("*************************** CLEVERGIRL CHATBOT ***************************")
+print("*************************** TYPE BYE WHEN DONE ***************************")
+print("-------------------- Conversation Started ---------------------------")
+print("")
+while True:
+    m = input("User: ")
+    print("Clevergirl: "+girl_says(m))
+    if 'bye' in m.lower():
+        break
+print("")
+print("-------------------- Conversation Ended -----------------------------")
+print("")

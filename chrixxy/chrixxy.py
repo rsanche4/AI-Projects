fe1ef584ab1191ae1fdce9702200ce6a6155a0ef
Rafael Sanchez <rsanche4@stevens.edu>
2022-06-18 11:23:34 -0400

    ch9

--- chrixxy/chrixxy.py
+++ chrixxy/chrixxy.py
@@ -20,8 +20,9 @@ else:
 POST_RATE = 10 # frequency at which it should post messages, the larger the number the less likely to randomly post a message
 TOKEN = ''
 
-ENDINGS = [":)", ":-)", "lol", ". . .", ";)", "(>.<)", ">_<", "hehehe", "hehe", "heeheehee", "heehee", "hahaha", "haha", "lmao", "lmaoo", "lmaooo", "yeah?", "also, ily", "omg", "tho"]
-WITH_ENDING_FREQ = 1
+BEGINNINGS = ["yeah", "umm...", "i mean", "like", "uwu", "lol", "hey", "yeah so", ". . .", "uh"]
+ENDINGS = [":)", ":-)", "lol", ". . .", ";)", "(>.<)", ">_<", "hehehe", "hehe", "heeheehee", "heehee", "hahaha", "haha", "lmao", "lmaoo", "lmaooo", "yeah?", "also, ily", "omg", "tho", "uwu"]
+WITH_FREQ = 1
 
 with open("corpus.txt", encoding="utf-8") as f:
     text = f.read()
@@ -41,16 +42,20 @@ async def on_message(message):
         text_model = markovify.Text(text)
         msg = text_model.make_short_sentence(random.randint(40, 50))
         msg = msg.replace(".", "")
-        if random.randint(0, WITH_ENDING_FREQ)==0:
+        if random.randint(0, WITH_FREQ)==0:
             msg = msg + " " + random.choice(ENDINGS)
+        if random.randint(0, WITH_FREQ)==0:
+            msg = msg + " " + random.choice(BEGINNINGS)
         await message.channel.send(msg.lower())
 
     if not message.guild:
         input_text = message.content.replace("@chrixxy", "")
         input_text = message.content.replace("chrixxy", "")
         response = k.respond(input_text)
-        if random.randint(0, WITH_ENDING_FREQ)==0:
+        if random.randint(0, WITH_FREQ)==0:
             response = response + " " + random.choice(ENDINGS)
+        if random.randint(0, WITH_FREQ)==0:
+            msg = msg + " " + random.choice(BEGINNINGS)
         await message.channel.send(response.lower())
 
 client.run(TOKEN)

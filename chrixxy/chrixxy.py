076a8b4660d97c71e37d830ae087651c114d3127
Rafael Sanchez <rsanche4@stevens.edu>
2022-06-25 14:58:36 -0400

    all of chrixxy and bd

--- chrixxy/chrixxy.py
+++ chrixxy/chrixxy.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2 import token
 import markovify
 import discord
 import random
@@ -6,6 +5,7 @@ import aiml
 import os
 from googlesearch import search
 import wikipedia
+from datetime import date, datetime
 
 BRAIN_FILE="brain.dump"
 
@@ -20,7 +20,7 @@ else:
     print("Saving brain file: " + BRAIN_FILE)
     k.saveBrain(BRAIN_FILE)
 
-POST_RATE = 10 # frequency at which it should post messages, the larger the number the less likely to randomly post a message
+POST_RATE = 7 # frequency at which it should post messages, the larger the number the less likely to randomly post a message
 tokenfile = open("C:\\Users\\rafas\\Documents\\chrixxytoken\\token.txt", "r")
 TOKEN = tokenfile.read()
 
@@ -28,7 +28,7 @@ BEGINNINGS = ["yeah", "umm...", "i mean", "like", "uwu", "lol", "hey", "yeah so"
 ENDINGS = [":)", ":-)", "lol", ". . .", ";)", "(>.<)", ">_<", "hehe", "hahaha", "haha", "lmao", "yeah?", "omg", "tho", "uwu", "yo", "bro"]
 WEBSEARCH_MESSAGE = ["found this :-)", "looked it up", "here ya go", "yessir", "found all this", "well, here is what i got", "maybe this will help", "looking for this?", "there ya go"]
 WITH_FREQ = 1
-NUM_OF_SEARCHURLS = 10
+NUM_OF_SEARCHURLS = 5
 
 with open("corpus.txt", encoding="utf-8") as f:
     text = f.read()
@@ -67,7 +67,41 @@ async def on_message(message):
 
     # help
     elif message.content.lower().startswith("plzhelp"):
-        await message.channel.send('I am the egirl bot. You can chat with me privately to get a more personal chat, \nbut I can also comment randomly on group chats, \nsearch things on the internet using the "search" command (ex "search tiktok egirl") in which case I will provide urls to pages I found related to the search. Look into google advanced searches for better results! \n and finally I can also provide a definition of phrases and words using wikipedia (ex: define love) \n lov u xoxo')
+        await message.channel.send('I am the egirl bot. You can chat with me privately to get a more personal chat, \nbut I can also comment randomly on group chats, \nsearch things on the internet using the "search" command (ex "search tiktok egirl") in which case I will provide urls to pages I found related to the search. Look into google advanced searches for better results! \nand finally I can also provide a definition of phrases and words using wikipedia (ex: define love) \nYou can also say "roll dice" to get a random number from 1 to 6, \nand you can ask for the time by including the word "time" in your message \nYou can choose to play a game called Blackjack Dice, in which I roll a 12 sided dice for you as often as possible until reaching a number higher than 16. Then I roll my dice and do the same, and if I get closer to 21 than you did, I win! Say, "blackjack dice" to start or just type "bd". \nlov u xoxo')
+
+    # roll dice
+    elif message.content.lower().startswith("roll dice"):
+        await message.channel.send(str(message.author) + " got: " + str(random.randint(1, 6)))
+
+    # tell the time
+    elif "time" in message.content.lower():
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        await message.channel.send("The time is " + current_time)
+
+    # the game blackjack dice
+    elif "blackjack dice" in message.content.lower() or "bd"==message.content.lower():
+        current_number = 0
+        chrixxy_number = 0
+        while current_number<16:
+            current_number += random.randint(1, 12)
+        if current_number>21:
+            await message.channel.send("You went over 21 by scoring " + str(current_number) + ". You lost!")
+        else:
+            await message.channel.send("You scored: "+ str(current_number) + "\nThrowing my dice now...")
+            while chrixxy_number<16:
+                chrixxy_number += random.randint(1, 12)
+            if chrixxy_number>21:
+                await message.channel.send("I went over 21 by scoring " + str(chrixxy_number) + ". You won!")
+            else:
+                if chrixxy_number>current_number:
+                    await message.channel.send("I am closer to 21 because I scored " + str(chrixxy_number) + ", I won!")
+                elif chrixxy_number<current_number:
+                    await message.channel.send("You are closer to 21, because I scored " + str(chrixxy_number) + ", you won!")
+                else:
+                    await message.channel.send("We have the same numer " + str(current_number) + "! Draw!!")
+
+
 
     # Conversation Functionality
     elif not message.guild:
@@ -84,8 +118,6 @@ async def on_message(message):
         # file.write(str(message.author) + ": " + str(message.content) + "\n")
         # file.write("chrixxy: " + str(response.lower()) + "\n")
         # file.close()    
-
-    
 
     if message.content=="XXSHUTDOWNXX":
         exit()
